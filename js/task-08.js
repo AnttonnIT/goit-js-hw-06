a1f5e9a6eef87a92bc19ed5166bd3264c5a44ed0
AnttonnIT <anttonnit@gmail.com>
2022-12-08 00:17:02 +0200

    up

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,29 @@
+"use strict";
+const form = document.querySelector(".login-form");
 
+const formSubmit = (event) => {
+  event.preventDefault();
+
+  const formElements = event.currentTarget.elements;
+
+  if (formElements.email.value === "" || formElements.password.value === "") {
+    alert("Будь ласка, заповніть всі поля");
+    return;
+  }
+
+  // const formData = new FormData(event.currentTarget);
+
+  const mail = formElements.email.value;
+  const password = formElements.password.value;
+
+  const formData = {
+    mail,
+    password,
+  };
+
+  console.log(formData);
+
+  form.reset();
+};
+
+form.addEventListener("submit", formSubmit);
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
+"use strict";
+
+const bodyEl = document.querySelector("body");
+const btnEl = document.querySelector(".change-color");
+const spanEl = document.querySelector(".color");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColor = () => {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+
+  spanEl.textContent = bodyEl.style.backgroundColor;
+};
+
+btnEl.addEventListener("click", changeColor);
